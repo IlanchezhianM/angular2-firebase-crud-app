471d4a91630b6a8e7864841f2e86c78a8145dac8
Sharvy Ahmed <sharvy2008@gmail.com>
2017-01-07 02:06:08 +0600

    Added Firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkrGjBqXHI71FztRVZDti7gwYSEDZvWwA",
+  authDomain: "fir-crud-6d67e.firebaseapp.com",
+  databaseURL: "https://fir-crud-6d67e.firebaseio.com",
+  storageBucket: "fir-crud-6d67e.appspot.com",
+  messagingSenderId: "185942809970"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
